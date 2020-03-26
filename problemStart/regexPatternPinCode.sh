7dc4d56fb583842b56d7d1f0de52d4619af594d1
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-27 01:44:29 +0530

    ADDED:Generated the india of pin code

--- problemStart/regexPatternPinCode.sh
+++ problemStart/regexPatternPinCode.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 #DISPLAY THE WELCOME STATEMENT
 echo "Welcome to the Constructs Patterns program"
+#WE ARE TAKING INPUT FROM USER
+read -p "Enter the number" pincode
+patternPinCode="^[1-9]{1}[0-9]{5}$"
+if [[ $pincode =~ $patternPinCode ]]
+then
+	echo "Valid"
+else
+	echo "Invlid"
+fi
 
