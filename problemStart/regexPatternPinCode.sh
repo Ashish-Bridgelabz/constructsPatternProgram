5ffe819b2416bd01e4bdac5023a73f008fa96bc8
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-28 00:13:06 +0530

    ADDED:Displayed the pin code using spice

--- problemStart/regexPatternPinCode.sh
+++ problemStart/regexPatternPinCode.sh
@@ -3,7 +3,7 @@ function pinCodeIndia()
 {
 #WE ARE TAKING INPUT FROM USER
 read -p "Enter the number" pincode
-patternPinCode="^[1-9]{1}[0-9]{5}$"
+patternPinCode="^[1-9]{1}[0-9]{2}\s?[0-9]{3}$"
 if [[ $pincode =~ $patternPinCode ]]
 then
 	echo "Valid"
