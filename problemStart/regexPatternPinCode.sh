add75308a6a97ea8f8aabf0785a94ee1f16ccd4c
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-28 00:06:26 +0530

    ADDED:Displayed the pin code

--- problemStart/regexPatternPinCode.sh
+++ problemStart/regexPatternPinCode.sh
@@ -1,13 +1,14 @@
-#!/bin/bash -x
-#DISPLAY THE WELCOME STATEMENT
-echo "Welcome to the Constructs Patterns program"
+#!/bin/bash
+function pinCodeIndia()
+{
 #WE ARE TAKING INPUT FROM USER
 read -p "Enter the number" pincode
-patternPinCode="^[a-zA-Z]{0}[1-9]{1}[0-9]{5}$"
+patternPinCode="^[1-9]{1}[0-9]{5}$"
 if [[ $pincode =~ $patternPinCode ]]
 then
 	echo "Valid"
 else
 	echo "Invlid"
 fi
-
+}
+pinCodeIndia
